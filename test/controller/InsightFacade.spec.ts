739a74a82bbff1d5e0c4f89119177543b50c0041
Frank Wang <fwang28@users.noreply.github.com>
2022-01-28 13:02:24 -0800

    added add dataset tests

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -58,15 +58,78 @@ describe("InsightFacade", function () {
 			fs.removeSync(persistDir);
 		});
 
-		// This is a unit test. You should create more like this!
 		it("Should add a valid dataset", function () {
 			const id: string = "courses";
-			const content: string = datasetContents.get("courses") ?? "";
+			const content: string = datasetContents.get(id) ?? "";
 			const expected: string[] = [id];
 			return insightFacade.addDataset(id, content, InsightDatasetKind.Courses).then((result: string[]) => {
 				expect(result).to.deep.equal(expected);
 			});
 		});
+
+		it("Should add two valid datasets", function () {
+			const id1: string = "courses";
+			const id2: string = "coursesmath";
+			const content1: string = datasetContents.get(id1) ?? "";
+			const content2: string = datasetContents.get(id2) ?? "";
+			const expected: string[] = [id1, id2];
+			return insightFacade.addDataset(id1, content1, InsightDatasetKind.Courses).then(() => {
+				return insightFacade.addDataset(id2, content2, InsightDatasetKind.Courses);
+			}).then((result: string[]) => {
+				expect(result).to.deep.equal(expected);
+			});
+		});
+
+		it("Should reject adding the same ID", async function () {
+			const id: string = "courses";
+			const content: string = datasetContents.get(id) ?? "";
+			try {
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+				expect.fail("Should have rejected!");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+				// Check to make sure that there is still one instance of courses in the dataset
+				return insightFacade.listDatasets().then((insightDatasets) => {
+					expect(insightDatasets).to.be.an.instanceof(Array);
+					expect(insightDatasets).to.have.length(1);
+				});
+			}
+		});
+
+		it ("Should reject adding an id with only whitespace", async function() {
+			const id: string = "   ";
+			const content: string = datasetContents.get("courses") ?? "";
+			try{
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+				expect.fail("Should have rejected!");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it ("Should reject adding an id with an underscore", async function() {
+			const id: string = "courses_underscore";
+			const content: string = datasetContents.get("courses") ?? "";
+			try{
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+				expect.fail("Should have rejected!");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
+		it ("Should reject adding an empty dataset", async function() {
+			const id: string = "emptyCourses";
+			const content: string = datasetContents.get(id) ?? "";
+			try{
+				await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+				expect.fail("Should have rejected!");
+			} catch (err) {
+				expect(err).to.be.instanceof(InsightError);
+			}
+		});
+
 	});
 
 	/*
